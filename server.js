8b488e9efa64f05f78ae97157558f90e06eafc14
Rayees Nadukkandy <rnadukkandy@sapient.com>
2018-09-08 18:41:41 +0530

    changing to heroku

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.post('/authenticate', (req, res) => {
   res.status(authData.status).send(authData.body)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, err => {
   if (err) {
     console.error(err)
